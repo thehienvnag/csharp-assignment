def4fca9ffcd2ff76441bd983c2ea73457b73cd0
thehienvnag <thehienvnag@gmail.com>
2020-07-26 07:43:05 +0700

    3rd commit: Add Login/Register feature.

---
+++ Models/AccountDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSharpAssignment.Models
+{
+    public class AccountDTO
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Fullname { get; set; }
+        public int StatusId { get; set; }
+        public string Email { get; set; }
+
+        public AccountDTO(int iD, string username, string fullname, int statusId, string email)
+        {
+            ID = iD;
+            Username = username;
+            Fullname = fullname;
+            StatusId = statusId;
+            Email = email;
+        }
+    }
+}
